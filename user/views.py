3f6968f3a794293652f0d9a3409d4a7c5363a757
Zihan <kanelmis@gmail.com>
2021-02-10 03:38:05 +0800

    10/02/2021

--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.urls import reverse,logout
+from django.urls import reverse
 from django.contrib.auth.views import LogoutView
 
 # Create your views here.
 def logout_view(request):
     """logout"""
-    logout(request)
+    LogoutView(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
 
